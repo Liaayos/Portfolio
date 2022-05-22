396d42c2ca9846f02ac2304db7367cb7e8cbd4c3
liaayos <ayo21002@byui.edu>
2022-05-21 22:39:13 -0500

    Added week 05

--- js/main.js
+++ js/main.js
@@ -16,6 +16,11 @@ const links = [{
     {
         label: "Week4 notes",
         url: "./weeks/week04.html"
+    },
+
+    {
+        label: "Week5 notes",
+        url: "./weeks/week05.html"
     }
 ]
 
